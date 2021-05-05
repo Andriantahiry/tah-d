613635dc4cfe42eb885deeba0c48d853a8bb26c6
Nomenahasina <luciochega@gmail.com>
2021-05-05 20:59:40 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// luxy.init();
+let menu = document.querySelector('.button-container');
+let trait = document.querySelector('.close');
+let items = document.querySelector('nav ul');
+
+trait.addEventListener('click', function () {
+    items.classList.add('hidden')
+})
+menu.addEventListener('click', function () {
+    if (items.classList.contains('hidden')) {
+        items.classList.remove('hidden')
+        gsap.from(items, {
+            autoAlpha: 1,
+            duration: 1,
+
+        });
+        gsap.to(menu, {
+            duration: 1,
+
+        })
+    }
+    else {
+        items.classList.add('hidden')
+    }
+});
+
+document.querySelector('.hero').addEventListener('click', function () {
+    items.classList.add('hidden')
+})
---
+++ trigger.js
@@ -0,0 +1,66 @@
+
+
+let tl = gsap.timeline();
+tl.from('.logo', {
+    duration: 1,
+    y: 100,
+
+})
+
+tl.to('.logo', {
+    duration: 1,
+    x: 50,
+
+})
+tl.from('.hero-title', {
+    duration: 1,
+    x: -200,
+    opacity: 0
+}, "-=1")
+tl.from('.hero-p', {
+    duration: 1,
+    x: -200,
+    opacity: 0
+}, "-=0.5")
+
+tl.from('.input-field', {
+    duration: 1,
+    x: -200,
+    opacity: 0
+}, "-=1.5")
+tl.from('.button-container', {
+    duration: 1,
+    x: -200,
+    opacity: 0
+})
+
+tl.from('#fb', {
+    duration: 1,
+    x: -400,
+    opacity: 0
+}, "-=1.5")
+
+tl.from('#lk', {
+    duration: 1,
+    x: -400,
+    opacity: 0
+}, "-=1")
+
+tl.from('#sk', {
+    duration: 1,
+    x: -400,
+    opacity: 0
+}, "-=1")
+
+
+
+
+let tr = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".image-design-container img",
+        start: "top center",
+        end: "bottom bottom",
+        toggleActions: "play none reverse reverse",
+
+    }
+});
